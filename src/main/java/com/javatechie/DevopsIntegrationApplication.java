486afabc5ef07851fd33ac0a70b3a75e86d18885
Sreeja <sreejasreekantan.gmail.com>
2023-05-24 12:16:57 +0530

    second commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to gitworld";
+		return "welcome to devops";
 	}
 
 	public static void main(String[] args) {
